e4b77f1062743cb17fd8e06cd8b6f7f84eed9773
Christian Bush <bushc325@gmail.com>
2024-03-16 02:08:52 -0400

    404 page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import NavBar from "./components/NavBar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import "./App.css";
+import NotFound from "./Pages/NotFound";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<ProjectLanding />} />
           <Route path="/link-shortener" element={<LinkShortenerApp />} />
+          <Route element={<NotFound />} />
         </Routes>
       </Router>
     </>
---
+++ src/Pages/NotFound.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+function NotFound() {
+  const navigate = useNavigate();
+  const [confirmClose, setConfirmClose] = useState(false);
+
+  const goBack = () => {
+    if (window.history.length === 1) {
+      setConfirmClose(true);
+    } else {
+      navigate(-1);
+    }
+  };
+
+  const toggleConfirmClose = () => {
+    setConfirmClose(!confirmClose);
+  };
+
+  const closeTab = () => {
+    window.close();
+  };
+
+  return (
+    <div className="flex h-screen w-screen flex-col items-center justify-center  text-3xl text-stone-200">
+      <h1>Error: 404</h1>
+      <h1 className="mb-5 border-b-2 border-green-400 pb-5">
+        That page does not exist
+      </h1>
+      <Link onClick={goBack} className="text-xl underline hover:text-green-400">
+        Go Back
+      </Link>
+      <div
+        className={`absolute top-6 flex flex-col items-center gap-3 rounded-md bg-stone-800 p-3 text-xl transition duration-200 ${confirmClose ? "" : "-translate-y-[150%]"}`}
+      >
+        <p>Close the tab?</p>
+        <div className="flex gap-10">
+          <button
+            onClick={closeTab}
+            className="min-w-28 bg-stone-600 opacity-50 hover:opacity-100"
+          >
+            YES
+          </button>
+          <button
+            onClick={() => setConfirmClose(false)}
+            className="min-w-28 bg-red-500 opacity-50 hover:opacity-100"
+          >
+            NO
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default NotFound;
