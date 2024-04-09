5bdd236d3dea35d961d201d0203bf74cab218f93
Christian Bush <bushc325@gmail.com>
2024-04-09 15:23:35 -0400

    revert to stable state

---
+++ src/Projects/WeatherApp/weatherBgLinks.js
@@ -0,0 +1,29 @@
+const thunderstorm =
+  "https://img.freepik.com/free-photo/view-apocalyptic-dark-clouds_23-2151065807.jpg?t=st=1712688193~exp=1712691793~hmac=db2e56a83cc9d4cb5a7ae81b6f9f40f9bf9b42b7367853981aa96e160ed3b6c4&w=1380";
+
+const drizzle =
+  "https://img.freepik.com/free-photo/variety-gradient-forms-copy-space-surface-bubbles_23-2148290036.jpg";
+
+const rain =
+  "https://img.freepik.com/free-photo/rain-drops-window_1339-7321.jpg?t=st=1712688971~exp=1712692571~hmac=286211b9c4c2a81e400bb42ef7377cb2e892f5c18cf191bbcae92ed5d7450ae9&w=740";
+
+const snow =
+  "https://img.freepik.com/free-photo/trees-covered-with-snow_107420-95711.jpg?t=st=1712688999~exp=1712692599~hmac=cb4a9553cdc6cc71c69028ede1801d421c883c44cff801fd37ffe7c0ce7706c8&w=740";
+
+const mist =
+  "https://img.freepik.com/free-photo/foggy-landscape_181624-9212.jpg?t=st=1712689170~exp=1712692770~hmac=b12c962a961d0bfb5d8ac920a9866ebeb0a1273caebf2c684f8e001da8765f75&w=740";
+const fog =
+  "https://img.freepik.com/free-photo/bare-trees-covered-with-fog_422131-48.jpg?t=st=1712689221~exp=1712692821~hmac=18bdbd2b2b12a9e2234b10a66600c057a85b9e468a2fdb2fe15a00d8522f1da8&w=740";
+
+const tornado =
+  "https://img.freepik.com/free-photo/view-apocalyptic-dark-stormy-clouds_23-2151065760.jpg?t=st=1712689252~exp=1712692852~hmac=0fd8489e85b469f0bedd0e5fce2aa42692f222b06911bac3163b04dcb686ca99&w=740";
+
+const clear =
+  "https://img.freepik.com/free-vector/bright-sun-sky_1053-219.jpg?w=740";
+const clearN =
+  "https://img.freepik.com/free-photo/panoramic-view-sunset_23-2148933766.jpg?t=st=1712689432~exp=1712693032~hmac=89d49ba724629233334e4e6fbff1b38f070bf94db56de7765c41e765ca6a9bda&w=740";
+
+const cloudy =
+  "https://img.freepik.com/free-photo/white-cloud-sky_1323-240.jpg?t=st=1712689392~exp=1712692992~hmac=b5f72f5ebb7016d75c3bb429320cf9981196ea6a32ed63585a96048621b57756&w=740";
+const cloudyN =
+  "https://img.freepik.com/free-photo/great-view-overcast-sky_1112-465.jpg?t=st=1712689486~exp=1712693086~hmac=aea45d8e9d6c2d662c19366868a3d57b62779f8bc7be7e70accf6b04ca51b332&w=740";
